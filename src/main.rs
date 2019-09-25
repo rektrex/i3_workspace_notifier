bcf30e57085f79c3f6ca3d9f1bad837c6bdbdbb7
rektrex <psaimanideep@gmail.com>
2019-09-25 13:38:20 +0530

    send notification using notify-send

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use i3ipc::I3Connection;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let mut connection = I3Connection::connect().unwrap();
+
+    let workspaces = connection.get_workspaces().unwrap().workspaces;
+
+    let mut notification = String::new();
+
+    let mut prev = 0;
+
+    for workspace in workspaces.iter() {
+        let diff = workspace.num - prev;
+        for _ in 1..diff {
+            notification += "<span foreground='#e76d6d'>●</span> "
+        }
+
+        if workspace.visible {
+            notification += "<span foreground='#88c563'>●</span> ";
+        } else {
+            notification += "● ";
+        }
+
+        prev = workspace.num;
+    }
+
+
+    Command::new("notify-send")
+            .arg("-u")
+            .arg("Low")
+            .arg(" ")
+            .arg(notification.trim_end())
+            .output()
+            .expect("command failed");
 }
