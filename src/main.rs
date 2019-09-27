f4647e99f35c8b7dd0385c9234da4cfb9ad05675
rektrex <psaimanideep@gmail.com>
2019-09-28 01:23:50 +0530

    read colors from a config file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 use i3ipc::I3Connection;
 use std::process::Command;
+use std::fs;
+use std::collections::HashMap;
+use dirs;
 
 fn main() {
+    let config_dir = dirs::config_dir().unwrap();
+    let config_path = config_dir.join("i3_workspace_notifier.conf");
+    let contents = fs::read_to_string(config_path).unwrap();
+
+    let mut colors = HashMap::new();
+
+    // default colors
+    colors.insert("visible", "#88c563");
+    colors.insert("non-visible", "#fafafa");
+    colors.insert("non-existent", "#e76d6d");
+
+    for line in contents.lines() {
+        if line.trim().is_empty() { continue; }
+
+        let pair: Vec<&str> = line.split(":").collect();
+        colors.insert(pair[0].trim(), pair[1].trim());
+    }
+
+    let visible_notification = format!("<span foreground='{}'>●</span> ", colors.get("visible").unwrap());
+    let non_visible_notification = format!("<span foreground='{}'>●</span> ", colors.get("non-visible").unwrap());
+    let non_existent_notification = format!("<span foreground='{}'>●</span> ", colors.get("non-existent").unwrap());
+
     let mut connection = I3Connection::connect().unwrap();
 
     let workspaces = connection.get_workspaces().unwrap().workspaces;
@@ -13,13 +38,13 @@ fn main() {
     for workspace in workspaces.iter() {
         let diff = workspace.num - prev;
         for _ in 1..diff {
-            notification += "<span foreground='#e76d6d'>●</span> "
+            notification += &non_existent_notification;
         }
 
         if workspace.visible {
-            notification += "<span foreground='#88c563'>●</span> ";
+            notification += &visible_notification;
         } else {
-            notification += "● ";
+            notification += &non_visible_notification;
         }
 
         prev = workspace.num;
